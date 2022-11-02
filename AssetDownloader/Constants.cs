b23f3629112ec86b694894d90bdc0eb41bf1d042
Jay Malhotra <jay0@me.com>
2022-11-02 13:32:55 +0000

    Enable memory mapping and increase concurrent file downloads

--- AssetDownloader/Constants.cs
+++ AssetDownloader/Constants.cs
@@ -10,7 +10,7 @@ public static class Constants
 
     // See: https://aria2.github.io/manual/en/html/aria2c.html#options
     public const string AriaOptions =
-        "-j5 --save-session-interval=120 --conditional-get=true --auto-file-renaming=false --summary-interval=0 --show-console-readout=false  --download-result=full";
+        "-j16 --enable-mmap --save-session-interval=120 --conditional-get=true --auto-file-renaming=false --summary-interval=0 --show-console-readout=false  --download-result=full";
 
     public const string RepoUrl =
         "https://codeload.github.com/CerisWhite/dl-datamine/zip/refs/heads/master";
